d56f7a94d9bfdfe74d0639b7a44811bebe5e712e
Olivier TABARY <olivier.tabary@smile.fr>
2019-02-07 11:00:44 +0100

    contrib

--- htdocs/typo3conf/ext/ho_content/ext_tables.php
+++ htdocs/typo3conf/ext/ho_content/ext_tables.php
@@ -11,6 +11,12 @@ call_user_func(
             'Bloc Vidéo'
         );
 
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            'Honest.HoContent',
+            'Pi2',
+            'Bloc Image'
+        );
+
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ho_content', 'Configuration/TypoScript', 'Plugin Content');
 
     }
@@ -26,11 +32,27 @@ $iconRegistry->registerIcon(
     ['source' => 'EXT:ho_content/Resources/Public/Icons/video.svg']
 );
 
+/**
+ * Register icons
+ */
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon(
+    'extension-honest-content-blocimage',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:ho_content/Resources/Public/Icons/image.svg']
+);
+
 // Pi1 Flexform
 $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
 $pluginSignature = strtolower($extensionName) . '_pi1';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/flexform_pi1.xml');
+
+// Pi2 Flexform
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+$pluginSignature = strtolower($extensionName) . '_pi2';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/flexform_pi2.xml');
 
 // ON CREE LE DOSSIER Correspondant AUX IMAGES
 if(!is_dir(__DIR__."/../../../uploads/tx_hocontent")){	@mkdir(__DIR__."/../../../uploads/tx_hocontent", 0775); }
